256acd370df1d9d58f0a97f3ea01cb3a888e743e
loufanglei <fanglei.lou@travelzen.com>
2015-04-21 13:29:56 +0800

    reinit

--- deploy-test3.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-selfpath=$(cd "$(dirname "$0")"; pwd)
-tops=$(cd "$(dirname "$selfpath/../../..")"; pwd)
-bin=$tops/script/auto_deploy.py
-$bin -t $tops -s /data/tops-front-hotel-chinaonline -w tops-front-hotel-chinaonline -p tops-hotel/tops-front-hotel-chinaonline -r test3 --host 192.168.164.159 -u hotel -a hotel -c tops-hotel.settings.gradle $*
